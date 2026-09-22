1fbbbe278584852f1cc7455ce06c7556835c7f96
ThammaratPul82 <thammarat827000@gmail.com>
2026-09-22 13:34:30 +0700

    finish

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -22,9 +22,15 @@ export default function RootLayout() {
           </div>
           <ModeToggle />
         </header>
+
         <main className="flex-1 p-4">
           <Outlet />
         </main>
+
+        {/* ข้อ 4: แก้เป็นชื่อ-นามสกุลจริงของตัวเอง */}
+        <footer className="border-t p-4 text-center text-xs text-muted-foreground">
+          จัดทำโดย ธรรมรัตน์ พูลถาวรสิน รหัสนักศึกษา 680610681
+        </footer>
       </SidebarInset>
     </SidebarProvider>
   );
